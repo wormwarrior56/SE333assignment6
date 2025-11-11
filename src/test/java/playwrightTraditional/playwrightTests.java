f8def746e88a65af8f281a2b18d434f30002291e
wormwarrior56 <superprotogentmail@proton.me>
2025-11-11 04:52:35 -0600

    headlessTrue

--- src/test/java/playwrightTraditional/playwrightTests.java
+++ src/test/java/playwrightTraditional/playwrightTests.java
@@ -24,7 +24,7 @@ public class playwrightTests {
     static void setup() {
         playwright = Playwright.create();
         browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
-                .setHeadless(false));
+                .setHeadless(true));
         context = browser.newContext(new Browser.NewContextOptions()
                 .setRecordVideoDir(Paths.get("videos/"))
                 .setRecordVideoSize(1280, 720));
